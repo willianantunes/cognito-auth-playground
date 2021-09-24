eda21d34cc5f499ff071f9d67959d88d40d458d1
Willian Antunes <willian.lima.antunes@gmail.com>
2021-09-24 18:59:53 -0300

    chore: initialize the project with readme, terraform, and more 🚀

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cognito Auth Playground
+
+Just playing a bit around with Cognito.
+
+I'm exploring, so do not consider this project as a real one (if you look at the code, you're going to laugh, haha). This is far to be considered a production-ready one.
---
+++ iac/main.tf
@@ -0,0 +1,132 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.60"
+    }
+  }
+
+  required_version = ">= 1.0.7"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+# Know more at: https://github.com/mineiros-io/terraform-aws-cognito-user-pool/blob/0ba192ca3af987887da092ee268af0981924dbae/examples/complete/main.tf
+module "cognito_user_pool" {
+  source  = "mineiros-io/cognito-user-pool/aws"
+  version = "0.7.0"
+
+  name = "poc-users-cognito-tmp"
+
+  # We allow the public to create user profiles
+  allow_admin_create_user_only = true
+
+  enable_username_case_sensitivity = false
+  advanced_security_mode           = "ENFORCED"
+
+  alias_attributes = [
+    "email",
+    "phone_number",
+    "preferred_username",
+  ]
+
+  auto_verified_attributes = [
+    "email"
+  ]
+
+  account_recovery_mechanisms = [
+    {
+      name     = "verified_email"
+      priority = 1
+    },
+    {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  ]
+
+  # If invited by an admin
+  invite_email_subject = "You've been invited to Mineiros.io"
+  invite_email_message = "Hi {username}, your temporary password is '{####}'."
+  invite_sms_message   = "Hi {username}, your temporary password is '{####}'."
+
+  domain                = "situation-auth-playground"
+  default_email_option  = "CONFIRM_WITH_LINK"
+  email_subject_by_link = "Your Verification Link"
+  email_message_by_link = "Please click the link below to verify your email address. {##Verify Email##}."
+  sms_message           = "Your verification code is {####}."
+
+  challenge_required_on_new_device = true
+  user_device_tracking             = "USER_OPT_IN"
+
+  # These paramters can be used to configure SES for emails
+  # email_sending_account  = "DEVELOPER"
+  # email_reply_to_address = "support@mineiros.io"
+  # email_from_address     = "noreply@mineiros.io"
+  # email_source_arn       = "arn:aws:ses:us-east-1:999999999999:identity"
+
+  # Require MFA
+  mfa_configuration        = "ON"
+  allow_software_mfa_token = true
+
+  password_minimum_length    = 40
+  password_require_lowercase = true
+  password_require_numbers   = true
+  password_require_uppercase = true
+  password_require_symbols   = true
+
+  temporary_password_validity_days = 3
+
+  schema_attributes = [
+    {
+      name       = "gender", # overwrites the default attribute 'gender'
+      type       = "String"
+      required   = true
+      min_length = 1
+      max_length = 2048
+    },
+    {
+      name                     = "alternative_name"
+      type                     = "String"
+      developer_only_attribute = false,
+      mutable                  = true,
+      required                 = false,
+      min_length               = 0,
+      max_length               = 2048
+    },
+    {
+      name      = "friends_count"
+      type      = "Number"
+      min_value = 0,
+      max_value = 100
+    },
+    {
+      name = "is_active"
+      type = "Boolean"
+
+    },
+    {
+      name = "last_seen"
+      type = "DateTime"
+    }
+  ]
+
+  clients = [
+    {
+      name                 = "android-mobile-client"
+      read_attributes      = ["email", "email_verified", "preferred_username"]
+      allowed_oauth_scopes = ["email", "openid"]
+      allowed_oauth_flows  = ["implicit"]
+      callback_urls        = ["https://mineiros.io/callback", "https://mineiros.io/anothercallback"]
+      default_redirect_uri = "https://mineiros.io/callback"
+      generate_secret      = true
+    }
+  ]
+
+  tags = {
+    environment = "Dev"
+  }
+}
